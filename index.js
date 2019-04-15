30e648c98a49fe896000f21708a2f0b65fc0c99d
HalfMomentum <mm305@snu.edu.in>
2019-04-15 23:01:16 +0530

    map added

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-
 const express = require('express');
 var app = express();
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-function myFunction(){
+function myFunction() {
   $(".footer-content").hide();
 }
 
-app.get("/api/maps",(req,res)=>{
+app.get("/api/maps", (req, res) => {
   var request = require("request");
 
-  var options = { method: 'GET',
-    url: 'https://maps.googleapis.com/maps/api/place/details/json',
-    qs:
-     { placeid: 'ChIJq6qqqk7rDDkRTIcYZ3USFDI',
-       fields: 'name,url,price_level,reviews,rating,photos,formatted_phone_number',
-       key: 'AIzaSyB-Ykp-FMb4dJasTNaooTKQ-ejJXEcldJY' },
-    headers:
-     { 'Postman-Token': '3a8dff97-fbc8-4cf3-95d3-3f3734f54091',
-       'cache-control': 'no-cache' } };
+  var options = {
+    method: 'GET',
+    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.6507,77.2334&radius=1500&rankby=prominence&type=restaurant&key=AIzaSyB3gKewQAoCtwAKnRXz3C1wDrp6xlFt3bI',
+    /*,
+        qs:
+         { placeid: 'ChIJq6qqqk7rDDkRTIcYZ3USFDI',
+           fields: 'name,url,price_level,reviews,rating,photos,formatted_phone_number',
+           key: 'AIzaSyB-Ykp-FMb4dJasTNaooTKQ-ejJXEcldJY' },*/
+    headers: {
+      'Postman-Token': '3a8dff97-fbc8-4cf3-95d3-3f3734f54091',
+      'cache-control': 'no-cache'
+    }
+  };
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
@@ -35,6 +38,6 @@ app.get("/api/maps",(req,res)=>{
 
 
 const PORT = 3333;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })
--- start.js
+++ start.js
@@ -1,25 +1,89 @@
   var app = new Vue({
     el: '#app',
     data: {
+      start: "",
+      end: "",
+      waypoint: "",
       hotels: [],
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      myLocation: {
+        lat: 28.6139,
+        lng: 77.2090
+      },
+      directionsService: null,
+      directionsDisplay: null
     },
     mounted() {
       $('.menu .item').tab();
       $('.ui.rating').rating('disable');
       this.get_hotels();
+      this.initMap();
     },
     methods: {
       get_hotels() {
         $.ajax({
           url: "http://localhost:3333/api/maps",
           success: (data) => {
-            var response = JSON.parse( data);
-            this.hotels = response.results;            
-            console.log(this.hotels[0]);
-            
+            var response = JSON.parse(data);
+            console.log(response);
+            this.hotels = response.results;
           }
         });
+      },
+      initMap() {
+        var mapCanvas = $('#map')[0];
+        this.directionsService = new google.maps.DirectionsService;
+        this.directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(mapCanvas, {
+          zoom: 11,
+          center: {
+            lat: 28.6139,
+            lng: 77.2090
+          }
+        });
+        this.directionsDisplay.setMap(map);
+        console.log("--some init value");
+
+        console.log(this.directionsDisplay);
+        console.log(this.directionsService);
+        console.log("some init value--");
+
+      },
+      getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(this.showPosition);
+        } else {
+          console.log("Not supported");
+        }
+      },
+      showPosition(position) {
+        this.myLocation.lat = position.coords.latitude;
+        this.myLocation.lng = position.coords.longitude;
+        console.log(this.myLocation);
+
+      },
+
+      calculateAndDisplayRoute() {
+        console.log(this.waypoint);
+        this.directionsService.route({
+            origin: this.start,
+            destination: this.end,
+            waypoints: this.waypoint,
+            optimizeWaypoints: true,
+            travelMode: 'DRIVING'
+          },
+          (response, status) => {
+            if (status === 'OK') {
+              directionsDisplay.setDirections(response);
+              var route = response.routes[0];
+              console.log(route);
+            } else {
+              console.log('Direction miss Direction');
+
+            }
+          }
+        )
+
       }
     },
   })
